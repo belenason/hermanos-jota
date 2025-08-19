6e10b791621faa7730815d0b5a4b1beff63ee3b5
Belen Ason <belenasons@gmail.com>
2025-08-18 21:02:37 -0300

    Setup inicial de la página web Hermanos Jota. Sitio web con integración de Bootstrap y estilos personalizados

---
+++ js/myjs.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("scroll", function() {
+    const navbar = document.querySelector(".custom-navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+        navbar.classList.remove("transparent");
+    } else {
+        navbar.classList.add("transparent");
+        navbar.classList.remove("scrolled");
+    }
+});
